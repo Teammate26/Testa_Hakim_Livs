ae799f415cb6c31097c5c736f7b8d01f32f7aeb6
Jesper Sjöberg <jesper26sjoberg@live.se>
2025-09-01 14:43:20 +0200

    Removed sensitive information in file

--- test_hakim_livs.py
+++ test_hakim_livs.py
@@ -1,8 +1,12 @@
 import re
 from playwright.sync_api import Page, expect
+from dotenv import load_dotenv
+import os
 
-username = "hakim@livs.se"
-password = "jagäradmin"
+load_dotenv()
+
+admin_username = os.getenv("ADMIN_USERNAME")
+admin_password = os.getenv("ADMIN_PASSWORD")
 
 
 def test_admin_add_product(page: Page):
@@ -11,8 +15,8 @@ def test_admin_add_product(page: Page):
     expect(page.get_by_text("Alla produkter")).to_be_visible()
     page.click("a:has-text('Logga in')")
 
-    page.fill("input[placeholder='E-postadress']", username)
-    page.fill("input[placeholder='Password']", password)
+    page.fill("input[placeholder='E-postadress']", admin_username)
+    page.fill("input[placeholder='Password']", admin_password)
     page.click("button:has-text('Logga in')")
     page.wait_for_load_state("networkidle")
 
